c0f40fcec0fdb3355f9971164c3017a30858c04d
montoto <montoto.munoz.javier@iescamas.es>
2023-10-17 10:09:00 +0200

    Creamos primeros modelos

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    birthdate = models.DateField()
+
+
+class Bar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
