1db72c6a66e673e43fd3ecf241ef054ea9c72bd3
montoto <montoto.munoz.javier@iescamas.es>
2023-10-21 13:53:13 +0200

    Agregar tabla anuncio, mesa, y reserva (junto con sus migraciones). Incluirlo en el admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from api.models import UserProfile, Bar
+from api.models import UserProfile, Bar, Advertisement, Table, Booking
 
 
 admin.site.register(UserProfile)
 admin.site.register(Bar)
+admin.site.register(Advertisement)
+admin.site.register(Table)
+admin.site.register(Booking)
--- backend/api/models.py
+++ backend/api/models.py
@@ -19,3 +19,42 @@ class Bar(models.Model):
     address = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+class Advertisement(models.Model):
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    reduction = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Table(models.Model):
+    class TableStatus(models.TextChoices):
+        FREE = ("FREE", "Free")
+        PENDING_OF_CONFIRMATION = ("PENDING_OF_CONFIRMATION", "Pending of confirmation")
+        BUSY = ("BUSY", "Busy")
+
+    seats = models.SmallIntegerField()
+    outdoor = models.BooleanField()
+    status = models.CharField(max_length=255, choices=TableStatus.choices, default=TableStatus.FREE)
+    number = models.SmallIntegerField()
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('number', 'bar',)
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    initial_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    completed = models.BooleanField()
+
+    class Meta:
+        unique_together = ('table', 'initial_datetime',)
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(end_datetime__gt=models.F('initial_datetime')),
+                name="end_datetime_check"
+            )
+        ]
