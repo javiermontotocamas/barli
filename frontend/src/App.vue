8fa8be7e5ae784340dd5875e085102d98be92f6e
montoto <montoto.munoz.javier@iescamas.es>
2023-10-31 12:02:51 +0100

    proyecto frontend generado con layout basico

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    Barli
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <div id="content">
+    <RouterView />
+  </div>
+
+  <footer>Pie de pagina</footer>
+</template>
+
+<style scoped>
+header {
+  background-color: beige;
+}
+
+#content {
+  flex: 1;
+}
+
+footer {
+  background-color: beige;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <p>Esto es el home</p>
+  </main>
+</template>
