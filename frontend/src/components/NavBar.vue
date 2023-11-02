89790557efc1101e773678ca1a2ed137dadccd89
montoto <montoto.munoz.javier@iescamas.es>
2023-11-02 13:23:25 +0100

    Integrar bootstrap y navbar estilo usando bootstrap

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,23 +3,46 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img src="https://placehold.co/64x64" />
-    <nav>
-        <router-link :to="{ name: 'home'}">Home</router-link> |
-        <router-link :to="{ name: 'about'}">About</router-link> |
-        <router-link :to="{ name: 'signin'}">SignIn</router-link> |
-        <router-link :to="{ name: 'register'}">Register</router-link>
-    </nav>
-  </header>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Barly</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'home' }" class="nav-link"
+                                router-link-active="active">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'about' }" class="nav-link"
+                                router-link-active="active">About</router-link>
+                        </li>
+                    </ul>
+                    <div class="d-flex gap-2">
+                        <router-link :to="{ name: 'signin' }" class="nav-link"
+                                router-link-active="active">
+                            <button class="btn btn-success">SignIn</button>
+                        </router-link>
+                        <router-link :to="{ name: 'register' }" tag="button">
+                            <button class="btn btn-info">Register</button>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <style scoped>
 header {
-  background-color: beige;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+    background-color: beige;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 }
 
 nav {
@@ -28,11 +51,11 @@ nav {
     flex: 1;
     justify-content: space-evenly;
 }
+
 #content {
-  flex: 1;
+    flex: 1;
 }
 
 footer {
-  background-color: beige;
-}
-</style>
+    background-color: beige;
+}</style>
